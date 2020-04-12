fd82ed74c37f9b769447c447d801ec460008eae7
DillonHeinen <56735131+DillonHeinen@users.noreply.github.com>
2020-04-11 23:40:58 -0500

    Initial Commit

---
+++ deck.cpp
@@ -0,0 +1,51 @@
+#include "time.h"
+#include "deck.h"
+
+using namespace std;
+
+Deck::Deck() {
+
+    srand(time(0));
+
+    for(int i = 0; i < 13; i++) {       // spades come first in deck
+        myCards[i] = Card(i + 1, Card::spades);
+    }
+    for(int i = 13; i < 26; i++) {      // then hearts
+        myCards[i] = Card(i + 1, Card::hearts);
+    }
+    for(int i = 26; i < 39; i++) {      // then diamonds
+        myCards[i] = Card(i + 1, Card::diamonds);
+    }
+    for(int i = 39; i < 52; i++) {      // then clubs
+        myCards[i] = Card(i + 1, Card::clubs);
+    }
+
+}
+
+void Deck::shuffle() {
+
+    for(int i = 0; i < SIZE*2; i++) {
+        int num1 = rand() % SIZE;
+        int num2 = rand() % SIZE;
+
+        Card temp = myCards[num1];
+        myCards[num1] = myCards[num2];
+        myCards[num2] = temp;
+    }
+
+}
+
+Card Deck::dealCard() {
+
+    while(myIndex < 52) {
+        myIndex++;
+        return myCards[myIndex - 1];
+    }
+
+}
+
+int Deck::size() const {
+
+    return SIZE - myIndex;
+
+}
