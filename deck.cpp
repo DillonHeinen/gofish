26881dfb2baffe6094c1d5f91d191bf23dcf4206
DillonHeinen <56735131+DillonHeinen@users.noreply.github.com>
2020-04-16 20:05:39 -0500

    Final Commit

--- deck.cpp
+++ deck.cpp
@@ -1,30 +1,30 @@
-#include "time.h"
+#include <ctime>
 #include "deck.h"
 
 using namespace std;
 
-Deck::Deck() {
-
+Deck::Deck() {          // fills deck using for loops to add in suit order
+    myIndex = 0;
     srand(time(0));
 
     for(int i = 0; i < 13; i++) {       // spades come first in deck
         myCards[i] = Card(i + 1, Card::spades);
     }
-    for(int i = 13; i < 26; i++) {      // then hearts
-        myCards[i] = Card(i + 1, Card::hearts);
+    for(int i = 0; i < 13; i++) {      // then hearts
+        myCards[i + 13] = Card(i + 1, Card::hearts);
     }
-    for(int i = 26; i < 39; i++) {      // then diamonds
-        myCards[i] = Card(i + 1, Card::diamonds);
+    for(int i = 0; i < 13; i++) {      // then diamonds
+        myCards[i + 26] = Card(i + 1, Card::diamonds);
     }
-    for(int i = 39; i < 52; i++) {      // then clubs
-        myCards[i] = Card(i + 1, Card::clubs);
+    for(int i = 0; i < 13; i++) {      // then clubs
+        myCards[i + 39] = Card(i + 1, Card::clubs);
     }
 
 }
 
 void Deck::shuffle() {
 
-    for(int i = 0; i < SIZE*2; i++) {
+    for(int i = 0; i < SIZE*2; i++) {           // switches two cards SIZE*2 times
         unsigned int num1 = rand() % SIZE;
         unsigned int num2 = rand() % SIZE;
 
@@ -35,15 +35,16 @@ void Deck::shuffle() {
 
 }
 
-Card Deck::dealCard() {
-
-    while(myIndex < 52) {
+Card Deck::dealCard() {            // returns top card on deck and increments index
+    if(myIndex < 52) {
         myIndex++;
         return myCards[myIndex - 1];
     }
-
+    else {
+        return myCards[0];
+    }
 }
 
-int Deck::size() const {
+int Deck::size() const {        // returns number of cards by using index location
     return SIZE - myIndex;
 }
