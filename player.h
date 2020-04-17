5f0835b9ce15fd4176cb75f270eb8768ad0b3803
DillonHeinen <56735131+DillonHeinen@users.noreply.github.com>
2020-04-16 20:08:02 -0500

    Final Commit

--- player.h
+++ player.h
@@ -13,18 +13,18 @@ class Player {
 
     Player();       // default constructor
 
-    Player(string name);
+    Player(string name);        // non-default constructor that takes name parameter
 
-    string getName() const;
+    string getName() const;         // returns name
 
     void addCard(Card c);     // adds a card to the hand
     void bookCards(Card c1, Card c2);     // books two cards
 
-    bool checkHandForBook(Card &c1, Card &c2);
+    bool checkHandForBook(Card &c1, Card &c2);        // checks if hand has two of same rank and populates parameter cards
 
-    bool rankInHand(Card c) const;
+    bool rankInHand(Card c) const;          // checks if player has rank in hand
 
-    Card chooseCardFromHand() const;
+    Card chooseCardFromHand() const;        // chooses random card for hand and returns it
 
     bool cardInHand(Card c) const;
 
@@ -33,7 +33,7 @@ class Player {
     string showHand() const;
     string showBooks()const;
 
-    int getHandSize() const;
+    int getHandSize() const;        // returns player's hand size in as integer
     int getBookSize() const;
 
     bool checkHandForPair(Card &c1, Card &c2);
